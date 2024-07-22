ac6cf95734c97dc6d00dff3758e3e9eb3b0f3ff2
CMB <info@generativelasers.be>
2024-07-22 14:00:52 +0200

    Introduce getCurrentFramePointCount() and update documentation

--- src/main/java/be/cmbsoft/laseroutput/LaserOutput.java
+++ src/main/java/be/cmbsoft/laseroutput/LaserOutput.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import be.cmbsoft.ilda.IldaFrame;
 import be.cmbsoft.ilda.IldaPoint;
 import be.cmbsoft.ilda.IldaRenderer;
-import processing.core.PGraphics;
+import processing.core.PImage;
 import processing.core.PVector;
 
 /**
@@ -16,7 +16,12 @@ import processing.core.PVector;
  */
 public abstract class LaserOutput extends Thread
 {
+    public enum Mode
+    {
+        STATIC_FPS, STATIC_PPS
+    }
     private final List<OutputOption> options              = new ArrayList<>();
+    private final Bounds bounds          = new Bounds();
     private       int                pps                  = 30000;
     private       boolean            paused;
     private       int                fps                  = 30;
@@ -25,9 +30,8 @@ public abstract class LaserOutput extends Thread
     private       int                lastFramePointCount  = 0;
     private       boolean            interrupted          = false;
     private       List<IldaPoint>    points               = new ArrayList<>();
-    private final Bounds             bounds               = new Bounds();
-    private       float              intensityFactor      = 1;
-    private       PGraphics          safetyZone           = null;
+    private       float  intensityFactor = 1;
+    private       PImage safetyZone      = null;
 
     protected LaserOutput()
     {
@@ -50,8 +54,7 @@ public abstract class LaserOutput extends Thread
                 long sleepTime = getSleepTime(lastTime, lastFramePointCount);
                 lastTime = System.currentTimeMillis();
                 sleep(sleepTime);
-            }
-            catch (InterruptedException exception)
+            } catch (InterruptedException exception)
             {
                 interrupt();
                 interrupted = true;
@@ -64,26 +67,24 @@ public abstract class LaserOutput extends Thread
         return (int) (intensityFactor * 255);
     }
 
-    private long getSleepTime(long lastTime, int lastFramePointCount)
+    /**
+     * Set “master” intensity (0-255)
+     *
+     * @param intensity
+     */
+    public void setIntensity(float intensity)
     {
-        long currentTime = System.currentTimeMillis();
-        switch (mode)
-        {
-            case STATIC_FPS:
-                return Math.max(0, millisecondsPerFrame - (currentTime - lastTime));
-            case STATIC_PPS:
-                long allottedFrameDuration = lastFramePointCount == 0 ? 33 : lastFramePointCount / pps;
-                return Math.max(0, allottedFrameDuration - (currentTime - lastTime));
-            default:
-                throw new IllegalStateException("Unexpected value: " + mode);
-        }
+        this.intensityFactor = intensity / 255f;
+    }
+
+    public int getCurrentFramePointCount()
+    {
+        return Optional.ofNullable(points).map(List::size).orElse(0);
     }
 
     public synchronized void setCurrentFrame(IldaFrame frame)
     {
-        this.points = Optional.ofNullable(frame)
-                              .map(IldaFrame::getPoints)
-                              .orElse(Collections.emptyList());
+        this.points = Optional.ofNullable(frame).map(IldaFrame::getPoints).orElse(Collections.emptyList());
     }
 
     public synchronized void setCurrentPoints(List<IldaPoint> points)
@@ -113,97 +114,27 @@ public abstract class LaserOutput extends Thread
         if (fps != 0)
         {
             millisecondsPerFrame = 1000L / fps;
-        }
-        else
+        } else
         {
             paused = true;
         }
     }
 
-    protected List<IldaPoint> transform(List<IldaPoint> points)
-    {
-        for (OutputOption option : options)
-        {
-            points = option.transform(points);
-        }
-        for (IldaPoint point : points)
-        {
-            int   colour             = point.getColour();
-            float theIntensityFactor = intensityFactor;
-            theIntensityFactor *= getSafetyZoneIntensity(point);
-
-            float red   = (colour >> 16) & 0xff;
-            float green = (colour >> 8) & 0xff;
-            float blue  = (colour) & 0xff;
-            point.setColour((int) (red * theIntensityFactor), (int) (green * theIntensityFactor),
-                (int) (blue * theIntensityFactor));
-        }
-        return applyBounds(points);
-    }
-
-    private float getSafetyZoneIntensity(IldaPoint point)
-    {
-        return safetyZone == null ? 1f : (safetyZone.get((int) ((point.getX() + 1) * (safetyZone.width - 1) / 2),
-            (int) ((-point.getY() + 1) * (safetyZone.height - 1) / 2)) & 0xff) / 255f;
-    }
-
-    public void setSafetyZone(PGraphics safetyZone)
+    /**
+     * Define a safety zone, also called beam attenuation map. The argument is an image, which will be rescaled to
+     * match the output aspect ratio. The blue value of the pixels corresponding to the location of a laser point,
+     * will be used to adjust the intensity of the laser at that location.
+     * <p>
+     * Beware! Only the intensity of the point is adjusted, not the intensity of the path to this point. If two
+     * points are being displayed, one at full power outside the defined safety zone and another well inside the
+     * zone, the beam will remain at full power along the path until it reaches the second point, even when the path
+     * crosses the safety zone boundary.
+     *
+     * @param safetyZone pixel grid as a PImage where the blue channel attenuates the intensity of the laser projection
+     */
+    public void setSafetyZone(PImage safetyZone)
     {
         this.safetyZone = safetyZone;
-    }
-
-    private List<IldaPoint> applyBounds(List<IldaPoint> points)
-    {
-
-        /*
-         * 4-points polynomial mapping algorithm courtesy of Daniele Mortari and David Arnas:
-         * Mortari, D.; Arnas, D. Bijective Mapping Analysis to Extend the Theory of Functional Connections to
-         * Non-Rectangular 2-Dimensional Domains. Mathematics 2020, 8, 1593. https://doi.org/10.3390/math8091593
-         */
-
-        List<IldaPoint> output = new ArrayList<>();
-        for (IldaPoint point : points)
-        {
-            PVector   position = point.getPosition();
-            float     newX     = calculateMapX(position.x, position.y);
-            float     newY     = calculateMapY(position.x, position.y);
-            IldaPoint newPoint = new IldaPoint(point);
-            newPoint.setPosition(newX, newY, point.getPosition().z);
-            output.add(newPoint);
-        }
-        return output;
-    }
-
-    private float calculateMapX(float a, float b)
-    {
-        return calculateMap(a, b, bounds.xk);
-    }
-
-    private float calculateMapY(float a, float b)
-    {
-        return calculateMap(a, b, bounds.yk);
-    }
-
-    private float calculateMap(float a, float b, float[] xyk)
-    {
-        float sum = 0;
-        for (int k = 0; k < 4; k++)
-        {
-            sum += xyk[k] * fk(a, b, k);
-        }
-        return sum;
-    }
-
-    private float fk(float a, float b, int k)
-    {
-        return switch (k)
-            {
-                case 0 -> (1 - a - b + a * b) * 0.25f;
-                case 1 -> (1 + a - b - a * b) * 0.25f;
-                case 2 -> (1 + a + b + a * b) * 0.25f;
-                case 3 -> (1 - a + b - a * b) * 0.25f;
-                default -> throw new IllegalArgumentException();
-            };
     }
 
     public abstract void project(List<IldaPoint> points);
@@ -215,7 +146,8 @@ public abstract class LaserOutput extends Thread
 
     public void project(IldaRenderer renderer)
     {
-        if (!interrupted) {
+        if (!interrupted)
+        {
             Optional.ofNullable(renderer)
                 .map(IldaRenderer::getCurrentFrame)
                 .ifPresent(frame -> this.points = frame.getPoints());
@@ -254,14 +186,10 @@ public abstract class LaserOutput extends Thread
 
     public abstract boolean isConnected();
 
-    public enum Mode
-    {
-        STATIC_FPS, STATIC_PPS
-    }
-
     /**
      * The Bounds, or Boundaries, are four vectors that define the quadrilateral figure in which projection occurs.
-     * Note that the figure does not need to be regular.
+     * Note that the figure does not need to be regular (rectangular): the image geometry will be transformed
+     * accordingly. Only polygons with 4 corners are supported at this moment.
      *
      * @return object containing the corners of the output shape
      */
@@ -270,14 +198,100 @@ public abstract class LaserOutput extends Thread
         return bounds;
     }
 
-    /**
-     * Set “master” intensity (0-255)
-     *
-     * @param intensity
-     */
-    public void setIntensity(float intensity)
+    private long getSleepTime(long lastTime, int lastFramePointCount)
     {
-        this.intensityFactor = intensity / 255f;
+        long currentTime = System.currentTimeMillis();
+        switch (mode)
+        {
+            case STATIC_FPS:
+                return Math.max(0, millisecondsPerFrame - (currentTime - lastTime));
+            case STATIC_PPS:
+                long allottedFrameDuration = lastFramePointCount == 0 ? 33 : lastFramePointCount / pps;
+                return Math.max(0, allottedFrameDuration - (currentTime - lastTime));
+            default:
+                throw new IllegalStateException("Unexpected value: " + mode);
+        }
+    }
+
+    private float getSafetyZoneIntensity(IldaPoint point)
+    {
+        return safetyZone == null ? 1f : (safetyZone.get((int) ((point.getX() + 1) * (safetyZone.width - 1) / 2),
+            (int) ((-point.getY() + 1) * (safetyZone.height - 1) / 2)) & 0xff) / 255f;
+    }
+
+    private List<IldaPoint> applyBounds(List<IldaPoint> points)
+    {
+
+        /*
+         * 4-points polynomial mapping algorithm courtesy of Daniele Mortari and David Arnas:
+         * Mortari, D.; Arnas, D. Bijective Mapping Analysis to Extend the Theory of Functional Connections to
+         * Non-Rectangular 2-Dimensional Domains. Mathematics 2020, 8, 1593. https://doi.org/10.3390/math8091593
+         */
+
+        List<IldaPoint> output = new ArrayList<>();
+        for (IldaPoint point: points)
+        {
+            PVector   position = point.getPosition();
+            float     newX     = calculateMapX(position.x, position.y);
+            float     newY     = calculateMapY(position.x, position.y);
+            IldaPoint newPoint = new IldaPoint(point);
+            newPoint.setPosition(newX, newY, point.getPosition().z);
+            output.add(newPoint);
+        }
+        return output;
+    }
+
+    private float calculateMapX(float a, float b)
+    {
+        return calculateMap(a, b, bounds.xk);
+    }
+
+    private float calculateMapY(float a, float b)
+    {
+        return calculateMap(a, b, bounds.yk);
+    }
+
+    private float calculateMap(float a, float b, float[] xyk)
+    {
+        float sum = 0;
+        for (int k = 0; k < 4; k++)
+        {
+            sum += xyk[k] * fk(a, b, k);
+        }
+        return sum;
+    }
+
+    private float fk(float a, float b, int k)
+    {
+        return switch (k)
+        {
+            case 0 -> (1 - a - b + a * b) * 0.25f;
+            case 1 -> (1 + a - b - a * b) * 0.25f;
+            case 2 -> (1 + a + b + a * b) * 0.25f;
+            case 3 -> (1 - a + b - a * b) * 0.25f;
+            default -> throw new IllegalArgumentException();
+        };
+    }
+
+    protected List<IldaPoint> transform(List<IldaPoint> points)
+    {
+        for (OutputOption option: options)
+        {
+            points = option.transform(points);
+        }
+        for (IldaPoint point: points)
+        {
+            int   colour             = point.getColour();
+            float theIntensityFactor = intensityFactor;
+            theIntensityFactor *= getSafetyZoneIntensity(point);
+
+            float red   = (colour >> 16) & 0xff;
+            float green = (colour >> 8) & 0xff;
+            float blue  = (colour) & 0xff;
+            point.setColour((int) (red * theIntensityFactor), (int) (green * theIntensityFactor),
+                (int) (blue * theIntensityFactor));
+        }
+        return applyBounds(points);
     }
 
 }
